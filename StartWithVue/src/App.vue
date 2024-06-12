df66f8edf9229bb1f1b2ec5f8182e01c4efec927
Abhishek Jha <3003abhishek.jha@gmail.com>
2024-06-12 12:27:14 +0530

    started with the learning vue

---
+++ StartWithVue/src/App.vue
@@ -0,0 +1,81 @@
+<script setup>
+
+import {reactive ,ref} from "vue" ;
+const counter = reactive({
+count : 0 ,
+}) ;
+console.log(counter.count) ;
+
+
+const message = ref("hello world") ;
+console.log(message.value)         ;
+
+const titleClass = ref("blueTitle");
+
+const text = ref("");
+
+const awesome = ref(true);
+
+
+function onToggle(){
+  awesome.value = !awesome.value ; 
+}
+
+
+function increment() {
+  counter.count++;
+}
+
+function onChange(e) {
+  console.log("Change hua bhai ") ; 
+  console.log(e.target.value);
+  text.value = e.target.value ; 
+}
+
+let id = 0 ; 
+const todos = ref([
+    {
+        id: id++,
+        text : "Learn Vue"
+    } , 
+     {
+        id: id++,
+        text : "Learn React"
+    } , 
+     {
+        id: id++,
+        text : "Learn React Native"
+    } , 
+])
+
+
+</script>
+<template>
+
+<div>
+<button @click = "increment" >Click me </button>
+<h1 :class = "titleClass">{{message }}</h1>
+<p>Count is : {{counter.count}}</p>
+<input @value = "text" @input = "onChange"/>
+<p>{{text}}</p>
+
+<button @click = "onToggle">Toggle</button>
+<p v-if = "awesome">Awesome</p>
+<p v-else >Oh-no</p>
+
+<ul v-for = "todo in todos "  :key = "todo.id">
+{{todo.text}}
+</ul>
+</div>
+
+</template>
+
+<style>
+
+.redTitle{
+color : red  ;
+}
+.blueTitle {
+color : blue ;
+}
+</style>
---
+++ StartWithVue/src/components /ListRendering.vue
@@ -0,0 +1,28 @@
+<script>
+import {ref} from 'vue' ; 
+
+let id = 0 ; 
+const todos = ref([
+    {
+        id: id++,
+        text : "Learn Vue"
+    } , 
+     {
+        id: id++,
+        text : "Learn React"
+    } , 
+     {
+        id: id++,
+        text : "Learn React Native"
+    } , 
+])
+
+</script>
+
+
+<template>
+
+<div v-for = "todo in todos ">
+<p>{{todo.text}}</p>
+</div>
+</template>
